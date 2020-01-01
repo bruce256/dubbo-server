03dd2db5e582f35996e6083f1fcd510bb56c6acc
LvSheng <326375225@qq.com>
2020-01-01 15:18:56 +0800

    可以启动了

--- dubbo-server-service/src/main/java/org/dubbo/server/service/DubboServer.java
+++ dubbo-server-service/src/main/java/org/dubbo/server/service/DubboServer.java
@@ -2,14 +2,19 @@ package org.dubbo.server.service;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.util.concurrent.CountDownLatch;
+
 
 public class DubboServer {
 	
-	public static void main(String[] args) throws Exception {  
+	public static void main(String[] args) throws Exception {
+		CountDownLatch cnl = new CountDownLatch(1);
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[] {"applicationContext.xml"});  
         context.start();  
         ExportService exportService = context.getBean("exportService", ExportService.class);
         exportService.export();
-        System.in.read(); // 为保证服务一直开着，利用输入流的阻塞来模拟  
+        System.out.println("provider started!");
+        cnl.await();
+        //System.in.read(); // 为保证服务一直开着，利用输入流的阻塞来模拟
     }  
 }
